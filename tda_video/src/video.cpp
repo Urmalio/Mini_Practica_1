892626a72c47e342a25e32731ea98eaa06eb3aa3
diego <diegodidilu@gmail.com>
2024-10-14 13:38:23 +0200

    implementacion de la mayoria de metodos de la clase video.cpp

--- tda_video/src/video.cpp
+++ tda_video/src/video.cpp
@@ -15,50 +15,52 @@ void read_directory(const std::string& name, vector<string>& v)
 }
 
 
-Video::Video(){
-    //COMPLETAR POR EL ESTUDIANTE
-}
+Video::Video(): seq(0){}
 /**************************************************/
-Video::Video(int n){
-//COMPLETAR POR EL ESTUDIANTE
-}
+Video::Video(int n): seq(n){}
 /**************************************************/
-Video::Video(const Video &V){
-//COMPLETAR POR EL ESTUDIANTE
-
-}
+Video::Video(const Video &V): seq(V.seq){}
 /**************************************************/
-Video::~Video(){}
+Video::~Video(){seq.clear();}
 /**************************************************/
 Video &Video::operator=(const Video &V){
-//COMPLETAR POR EL ESTUDIANTE
+    if (seq != V.seq) seq = V.seq;
+    return *this;
 }
 /**************************************************/
-int Video::size() const{
-    //COMPLETAR POR EL ESTUDIANTE
-}
+int Video::size() const{return seq.size();}
 /**************************************************/
-Image &Video::operator[](int foto){
-    //COMPLETAR POR EL ESTUDIANTE
-}
+Image &Video::operator[](int foto){return seq[foto];}
 
-const Image &Video::operator[](int foto)const{
-    //COMPLETAR POR EL ESTUDIANTE
-}
+const Image &Video::operator[](int foto)const{return seq[foto];}
 
-void Video::Insertar(int k, const Image &I){
-    //COMPLETAR POR EL ESTUDIANTE
+void Video::Insertar(int k, const Image &I){seq.insert(seq.begin() + k, I);}
 
-}
-
-void Video::Borrar(int k){
-    //COMPLETAR POR EL ESTUDIANTE
-}
+void Video::Borrar(int k){seq.erase( seq.begin() + k );}
 
 bool Video::LeerVideo(const string &path){
 
-    //USA read_directory PARA LEER los fichero de un directorio
-    //COMPLETAR POR EL ESTUDIANTE
+    vector<string> v;
+    read_directory(path, v);
+
+    seq.clear();
+
+
+    bool lectura_correcta = true;
+    int i=0;
+    Image tmp_image;
+
+    while (lectura_correcta && i<size())
+    {
+        lectura_correcta = tmp_image.Load( v[i].c_str() );
+        if (lectura_correcta)
+        {
+            seq.push_back( tmp_image );
+            ++i;
+        }
+    }
+
+    return lectura_correcta;
 }
 
 bool Video::EscribirVideo(const string & path, const string &prefijo)const{
@@ -79,6 +81,16 @@ bool Video::EscribirVideo(const string & path, const string &prefijo)const{
         }
     }
 
-    //COMPLETAR POR EL ESTUDIANTE
+    int i=0;
+    string base = "/"+prefijo+"_";
+    while (i<size() && exito){
+        exito = (*this)[i].Save(path+base+);
+
+        if (exito)
+        {
+            ++i;
+        }
+
+    }
 
 }
