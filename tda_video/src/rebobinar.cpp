4480a23bc5d04c25bf4402f7651b40e354441e75
L <liyaojian0811@gmail.com>
2024-10-18 19:52:34 +0200

    Añadí comentarios y algo más compacto el código

--- tda_video/src/rebobinar.cpp
+++ tda_video/src/rebobinar.cpp
@@ -3,7 +3,11 @@
 #include <iostream>
 
 using namespace std;
-
+/**
+* @brief Función que rebobina video
+* @param V el video original que va a servir de referencia para rebobinar
+* @post El objeto creado tendrá las mismas imágenes de V pero al revés
+*/
 Video Rebobinar(const Video &V)
 {
   int n = v.size();
@@ -14,27 +18,35 @@ Video Rebobinar(const Video &V)
   return aux;
 }
 
+/**
+* @brief Función que muestra mensaje error
+* @param os El flujo de salida que se usará
+* @post Muestra el formato correcto que se debe escribir para ejecutar el programa
+*/
 void Error(ostream &os){
   os << "Formato para ejecutar el programa:";
   os << "./programa /directorio_entrada /directorio_salida";
 }
 
+// Main
 int main(int argc, char * argv[]){
+  // Comprobar números de argumentos
   if(argc!=3)
     Error(cerr);
-  
-  bool is_directory = filesystem::is_directory(argv[1]) && 
-                      filesystem::is_directory(argv[2]);
-  if(!is_directory)
+
+  if(!(filesystem::is_directory(argv[1]) && 
+      filesystem::is_directory(argv[2])))
     Error(cerr);
 
   const string PREFIJO = "fotograma";
-  Video v_e,v_s;
+  Video v_e;
 
+  // Si se pudo hacer la lectura correctamente
   if(v_e.LeerVideo(argv[1]))
-    v_s = rebobinar(v_e);
-
-  v_s.EscribirVideo(argv[2], PREFIJO);
+  {
+    v_e = rebobinar(v_e);
+    v_e.EscribirVideo(argv[2], PREFIJO);
+  }
 
   return 0;
 }
